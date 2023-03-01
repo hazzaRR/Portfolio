5581599bc485bef13123a23602459d238dad5f2b
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-03-01 22:48:31 +0000

    added experience section with names and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     telemetry: false,
-    modules: [
-          '@nuxtjs/tailwindcss'
-        ],
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+      head: {
+        title: 'Harry Redman',
+        link: [
+          {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css'}
+        ]
+      }
+    }
+    
 
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
 <template>
     <div>
         <PersonalHero />
-
+        <div class="pb-16">
+            <h1 class="text-2xl font-bold text-center lg:text-left">Experience</h1>
+            <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 justify-evenly">
+                <div class="text-center" v-for="experience in experiences" v-bind:key="experience">
+                    <i class="text-7xl" :class="experience.icon "></i>
+                    <p class="font-bold">{{ experience.name }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+
+
+
+const experiences = {
+    python: {name:"Python", icon:"devicon-python-plain colored"},
+    java: {name:"Java", icon:"devicon-java-plain colored"},
+    javascript: {name:"JavaScript", icon:"devicon-javascript-plain colored"},
+    nuxt: {name:"Nuxt JS", icon:"devicon-nuxtjs-plain colored"},
+    vue: {name:"Vue JS", icon:"devicon-vuejs-plain colored"},
+    postgres: {name:"Postgres", icon:"devicon-postgresql-plain colored"},
+    git: {name:"Git", icon:"devicon-git-plain colored"},
+    html: {name:"HTML", icon:"devicon-html5-plain colored"},
+    css: {name:"CSS", icon:"devicon-css3-plain colored"},
+    tailwind: {name:"Tailwind CSS", icon:"devicon-tailwindcss-plain colored"},
+    express: {name:"Express JS", icon:"devicon-express-original colored"},
+    github: {name:"Github", icon:"devicon-github-original colored"},
+}
 
 </script>
 
