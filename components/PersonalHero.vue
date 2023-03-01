4f954cf68df48508e16af45d2762e45cf9c81030
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-03-01 19:55:12 +0000

    made navbar and and personalhero elements for website

---
+++ components/PersonalHero.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+        <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="/profile_photo_Edited.jpg" class="max-w-sm shadow-2xl h-3/12 w-3/12 rounded-full hover:w-full hover:h-full m-16" />
+            <div>
+            <h1 class="text-5xl font-bold text-center lg:text-left">Harry Redman</h1>
+            <p class="py-6 text-center lg:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum fuga assumenda vitae possimus, commodi ipsum ipsa eius quasi labore tempora sed, qui facere obcaecati velit. Sequi ex sed repudiandae distinctio totam exercitationem iste delectus. Aperiam expedita est velit, accusamus suscipit ad voluptatibus minus. Quibusdam vitae modi praesentium, quis labore sequi perspiciatis minus, rem aut adipisci accusamus ratione in fuga provident eum animi repudiandae ea? Debitis sed accusamus dolore provident eum vel. Vero totam iusto suscipit magnam optio, architecto quidem a quibusdam. Deleniti harum sed eos inventore repudiandae natus molestiae odit iusto, soluta, aut nostrum praesentium necessitatibus! Atque doloribus odio fuga.</p>
+            <!-- <button class="btn btn-primary">Get Started</button> -->
+            <div>
+                <p>hey</p>
+                <p>d</p>
+            </div>
+            </div>
+        </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <PersonalHero />
+        <h1>Projects Page</h1>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
