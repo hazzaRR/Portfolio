5cd6cbe90acb0cf59a8b32bbabf385f614a76327
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-03-15 18:13:29 +0000

    added images to the public folder to fix rendering issue

--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1678903012615}
+export default {data:{},prerenderedAt:1678903976366}
--- projects/_payload.js
+++ projects/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1678903012633}
+export default {data:{},prerenderedAt:1678903976375}
