9e49dca37bbf9387377c1c5f2a582b2cfe4cfff1
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-12-02 15:11:36 +0000

    updated bio for website and profile picture

--- components/PersonalHero.vue
+++ components/PersonalHero.vue
@@ -3,13 +3,14 @@
 
         <div class="hero min-h-screen bg-base-100">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="/profile_photo_Edited.jpg" class="max-w-sm shadow-2xl h-3/12 w-3/12 rounded-full hover:w-full hover:h-full m-16" />
+            <img src="/profile_photo_Edited.jpg" class="max-w-sm shadow-2xl w-full h-full m-16" />
             <div>
             <h1 class="text-5xl font-bold text-center lg:text-left">Harry Redman</h1>
             <p class="py-6 text-center lg:text-left">
-                Hi, my name is Harry and I am currently a third year Computer Science student studying at the University of East Anglia. I am an aspiring software developer who has a particular interest in machine learning,
-                 mobile development and Website development. Outside of coding I also enjoy keeping active whether thats going to the gym, playing football or running. I also enjoy watching sports in particular
-                 Formula 1 and football.
+                Hello, my name is Harry, and I graduated from the University of East Anglia with a first-class (Hons)
+                degree in computer science. I am a budding software engineer with a passion for machine learning, mobile development, and website development. 
+                Aside from coding, I enjoy being active by going to the gym, playing football, or running. I also enjoy watching sports, especially Formula One and football. 
+                One interesting fact about myself is that I am a type 1 diabetic.
             </p>
             <div>
             </div>
