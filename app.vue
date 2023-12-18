fdea3787a3cbf3bb4ed42b87a9cb6c5a301ebc82
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-12-18 21:00:24 +0000

    added trips section to website

--- app.vue
+++ app.vue
@@ -2,18 +2,21 @@
 
 const links = [
 {name: "Home", link: "/"},
-{name: "Projects", link: "/projects"}
+{name: "Projects", link: "/projects"},
+{name: "Trips", link: "/trips"}
 ]
 
 </script>
 
 <template>
-  <Navbar :links="links" />
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
-      <Footer />
-</template>
+  <div class="min-h-screen flex flex-col">
+    <Navbar class="sticky top-0 z-50" :links="links" />
+    <NuxtLayout>
+      <NuxtPage class="grow bg-neutral-100 dark:bg-neutral min-h-screen" />
+    </NuxtLayout>
+    <Footer />
+  </div>
+  </template>
 
 <style scoped>
 </style>
---
+++ pages/trips.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center">
+
+        <div class="container px-6 py-10 mx-auto">
+        <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Travel Journeys</h1>
+        <p class="mt-4 text-center text-gray-500 dark:text-gray-300">
+            A collection of all my travel journeys I have recorded using polarsteps
+        </p>
+        
+        <iframe class="w-full md:w-6/12 mx-auto aspect-video mt-10" src="https://www.polarsteps.com/HarryRedman/8870729-south-east-asia/embed"></iframe>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
