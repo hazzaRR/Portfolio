b163e1cac98387075e92b4a67ada714dee18e8a4
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-01-27 13:19:55 +0000

    added custom notfound error page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <NotFound v-if="error.statusCode" />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['error'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
