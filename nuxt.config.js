794328a43199bbfb4bf6f46ea9357fe8f3c5c525
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-01-27 08:59:27 +0000

    added link to favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default defineNuxtConfig({
         link: [
           {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css'},
           {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/daisyui@2.51.4/dist/full.css', type:'text/css'},
-          {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css', type:'text/css'}
+          {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css', type:'text/css'},
+          {rel: 'icon', type: 'image/x-icon', href: './public/favicon.ico'}
 
         ],
       },
