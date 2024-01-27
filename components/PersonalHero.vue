85fe191fe3ccabd855fb82a828f7627da9e9a5eb
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-01-27 12:49:33 +0000

    added download cv button to webpage

--- components/PersonalHero.vue
+++ components/PersonalHero.vue
@@ -2,33 +2,51 @@
     <div class="hero min-h-screen bg-neutral-100 dark:bg-neutral">
         <div class="hero-content flex-col lg:flex-row">
             <img src="/images/profile_photo_Edited.jpg" class="max-w-sm shadow-2xl w-full h-full m-16" />
-        <div class="">
-            <h1 class="text-5xl font-bold text-center lg:text-left">Harry Redman</h1>
-            <p class="py-6 text-center lg:text-left">
-                Hello, my name is Harry, and I recently graduated from the University of East Anglia with a first-class (Hons)
-                degree in Computer Science. I am a aspiring software engineer currently based in Derby with a passion for machine learning, mobile development, and website development. 
-                Aside from coding, I enjoy being active by going to the gym, playing football, or running. I also enjoy watching sports, especially Formula One and football. 
-                One interesting fact about myself is that I am a type 1 diabetic.
-            </p>
-            <div class="flex flex-col items-center lg:items-start mt-6">
-                    
-            <a href="https://www.linkedin.com/in/harryredman/" class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Linkedin">
-                <div class="flex items-center m-1">
-                    <i class="devicon-linkedin-plain text-xl"></i>
-                    <p class="ml-2 mb-1">/in/harryredman/</p>
-                </div>
-            </a>
-            
-            <a href="https://github.com/hazzaRR" class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Github">
-                <div class="flex items-center m-1">                
-                <i class="devicon-github-original text-xl"></i>
-                <p class="ml-2 mb-1">github.com/hazzaRR/</p>
-                </div>
-            </a>
+            <div class="">
+                <h1 class="text-5xl font-bold text-center lg:text-left">Harry Redman</h1>
+                <p class="py-6 text-center lg:text-left">
+                    Hello, my name is Harry, and I recently graduated from the University of East Anglia with a first-class
+                    (Hons)
+                    degree in Computer Science. I am a aspiring software engineer currently based in Derby with a passion
+                    for machine learning, mobile development, and website development.
+                    Aside from coding, I enjoy being active by going to the gym, playing football, or running. I also enjoy
+                    watching sports, especially Formula One and football.
+                    One interesting fact about myself is that I am a type 1 diabetic.
+                </p>
+                <div class="flex flex-col items-center lg:items-start mt-6">
+
+                    <a href="https://www.linkedin.com/in/harryredman/"
+                        class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                        aria-label="Linkedin">
+                        <div class="flex items-center m-1">
+                            <i class="devicon-linkedin-plain text-xl"></i>
+                            <p class="ml-2 mb-1">/in/harryredman/</p>
+                        </div>
+                    </a>
+
+                    <a href="https://github.com/hazzaRR"
+                        class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                        aria-label="Github">
+                        <div class="flex items-center m-1">
+                            <i class="devicon-github-original text-xl"></i>
+                            <p class="ml-2 mb-1">github.com/hazzaRR/</p>
+                        </div>
+                    </a>
+
+                        <a
+                        href="/Harry_Redman_Software_Dev.pdf"
+                        download=""
+                            class="m-2 flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                            </svg>
+                            <span>Download CV</span>
+                        </a>
             </div>
         </div>
-        
+
     </div>
 
- </div>
-</template>
+</div></template>
--- pages/projects/[project].vue
+++ pages/projects/[project].vue
@@ -7,7 +7,6 @@
         <div class="mt-8">
             <!-- <img class="object-cover md:w-1/2 w-full mx-auto rounded-xl h-72 lg:h-96 mb-6" :src="project.images[0].url" :alt="project.images[0].alt"> -->
 
-
             <ImageSlider class="object-cover md:w-1/2 w-full mx-auto rounded-xl h-72 lg:h-96" :images="project.images" :url="project.url"/>
 
             <div class="mt-6 mx-auto md:w-1/2 w-full">
