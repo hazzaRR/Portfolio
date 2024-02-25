382fda91cbb78d303168db921ba64d1b791ae08a
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-02-25 14:56:01 +0000

    added projects using json file and made some styling changes and conditonal renders

--- components/OnGoingProjects.vue
+++ components/OnGoingProjects.vue
@@ -39,11 +39,16 @@
                                     </div>
                                 </td>
                                 <td class="px-12 py-4 text-sm font-medium whitespace-nowrap">
-                                    <div class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
-                                        {{ project.started }}
-                                    </div>
-                                </td>
-                                <td class="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div v-if="project.started"
+                                            class="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                                            Started
+                                        </div>
+                                        <div v-else
+                                            class="inline px-3 py-1 text-sm font-normal text-gray-500 bg-gray-100 rounded-full dark:text-gray-400 gap-x-2 dark:bg-gray-800">
+                                            Planned
+                                        </div>
+                                    </td>
+                                <td class="px-4 py-4 text-sm">
                                     <div>
                                         <h4 class="text-gray-700 dark:text-gray-200">{{project.descriptionTitle}}</h4>
                                         <p class="text-gray-500 dark:text-gray-400">{{project.description}}</p>
@@ -52,13 +57,13 @@
 
                                 <td class="px-4 py-4 text-sm whitespace-nowrap">
                                     <div class="flex items-center gap-x-2">
-                                        <p v-for="(technology, index) in project.technologies" :key="index" class="px-3 py-1 text-xs text-blue-500 rounded-full dark:bg-gray-800 bg-blue-100/60">technology</p>
+                                        <p v-for="(technology, index) in project.technologies" :key="index" class="px-3 py-1 text-xs text-blue-500 rounded-full dark:bg-gray-800 bg-blue-100/60">{{technology}}</p>
                                     </div>
                                 </td>
 
                                 <td class="px-4 py-4 text-sm whitespace-nowrap">
                                     <div class="w-48 h-1.5 bg-blue-200 overflow-hidden rounded-full">
-                                        <div class="bg-blue-500 w-2/3 h-1.5"></div>
+                                        <div :class="['bg-blue-500', project.progress, 'h-1.5']"></div>
                                     </div>
                                 </td>
 
